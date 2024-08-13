dfc651f2975d1ece0cca0984cb6a6bfa07c61b12
mohd shamail <coolshamail4@gmail.com>
2024-08-13 13:31:12 +0530

    added config

--- controllers/accounts/downloadFeesInvoice.js
+++ controllers/accounts/downloadFeesInvoice.js
@@ -5,13 +5,24 @@ const CustomErrorHandler = require("../../services/CustomErrorHandler");
 const puppeteer = require("puppeteer");
 
 const generatePDF = async (htmlContent) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    executablePath: '/opt/render/.cache/puppeteer/chrome/linux-127.0.6533.99/chrome',
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--no-zygote",
+    ],
+  });
+
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
   const pdfBuffer = await page.pdf({ format: "A4" });
   await browser.close();
   return pdfBuffer.toString("base64");
-};
+}
 
 const feesInvoiceController = {
   async downloadInvoice(req, res, next) {
