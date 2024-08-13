0df1e19e9e31d6e578fa1da558c4c815f052fc0d
mohd shamail <coolshamail4@gmail.com>
2024-08-13 13:53:21 +0530

    added new feature

--- controllers/accounts/downloadFeesInvoice.js
+++ controllers/accounts/downloadFeesInvoice.js
@@ -5,18 +5,7 @@ const CustomErrorHandler = require("../../services/CustomErrorHandler");
 const puppeteer = require("puppeteer");
 
 const generatePDF = async (htmlContent) => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: '/opt/render/.cache/puppeteer/chrome/linux-127.0.6533.99/chrome-linux64/chrome',
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-gpu",
-      "--no-zygote",
-    ],
-  });
-
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
   const pdfBuffer = await page.pdf({ format: "A4" });
